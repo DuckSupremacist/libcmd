c00decc7315716005561ec4c48f9af753f824f3d
DuckSupremacist <theo@absolut-sensing.com>
2025-10-23 17:30:31 +0200

    more static tests for concepts

--- tests/src/meta_handler.test.cpp
+++ tests/src/meta_handler.test.cpp
@@ -1,22 +1,29 @@
-#include <gtest/gtest.h>
-#include <vector>
 #include <cstdint>
+#include <gtest/gtest.h>
 #include <string>
+#include <vector>
 
-#include "meta_handler.h"
 #include "communication.h"
+#include "meta_handler.h"
 
 /* ―――――――――――――――― Test doubles ―――――――――――――――― */
 
-class DummyCommunicator final : public Communicator {
-public:
-    void respond(const std::vector<std::uint8_t>& r) const override { last_response = r; ++respond_calls; }
+class DummyCommunicator final : public Communicator
+{
+  public:
+    void respond(const std::vector<std::uint8_t>& data) const override {
+        last_response = data;
+        ++respond_calls;
+    }
 
-    REQUEST_STATUS request(const std::vector<std::uint8_t>& message,
-                           const std::function<void(std::vector<std::uint8_t>)> handle_response_callback) const override {
+    REQUEST_STATUS request(
+        const std::vector<std::uint8_t>& message,
+        const std::function<void(std::vector<std::uint8_t>)> handle_response_callback
+    ) const override {
         last_request = message;
         ++request_calls;
-        for (const std::vector<std::uint8_t>& responses : scripted_responses) handle_response_callback(responses);
+        for (const std::vector<std::uint8_t>& responses : scripted_responses)
+            handle_response_callback(responses);
         return scripted_status;
     }
 
@@ -31,12 +38,12 @@ public:
 
 /* ―――――――――――――――― Fake Handlers ―――――――――――――――― */
 
-struct OkHandlerA {
+struct OkHandlerA
+{
     static constexpr std::uint8_t ID = 0x01;
     inline static int _calls = 0;
 
-    static HandlerExecuteResult
-    execute(const std::vector<std::uint8_t>& data, const Communicator& comm) {
+    static HandlerExecuteResult execute(const std::vector<std::uint8_t>& data, const Communicator& comm) {
         (void)comm;
         _last_data = data;
         ++_calls;
@@ -45,14 +52,13 @@ struct OkHandlerA {
 
     inline static std::vector<std::uint8_t> _last_data{};
 };
-static_assert(HandlerLike<OkHandlerA>);
 
-struct OkHandlerB {
+struct OkHandlerB
+{
     static constexpr std::uint8_t ID = 0x02;
     inline static int _calls = 0;
 
-    static HandlerExecuteResult
-    execute(const std::vector<std::uint8_t>& data, const Communicator& comm) {
+    static HandlerExecuteResult execute(const std::vector<std::uint8_t>& data, const Communicator& comm) {
         (void)comm;
         _last_data = data;
         ++_calls;
@@ -61,27 +67,58 @@ struct OkHandlerB {
 
     inline static std::vector<std::uint8_t> _last_data{};
 };
-static_assert(HandlerLike<OkHandlerB>);
 
-struct ErrHandler {
+struct ErrHandler
+{
     static constexpr std::uint8_t ID = 0x03;
     inline static int _calls = 0;
 
-    static HandlerExecuteResult
-    execute(const std::vector<std::uint8_t>&, const Communicator&) {
+    static HandlerExecuteResult execute(const std::vector<std::uint8_t>&, const Communicator&) {
         ++_calls;
-        return unexpected(HandlerExecuteError{
-            .code = HANDLER_EXECUTE_STATUS::ERROR_ID_NOT_FOUND,
-            .msg  = "boom"
-        });
+        return unexpected(HandlerExecuteError{.code = HANDLER_EXECUTE_STATUS::ERROR_ID_NOT_FOUND, .msg = "boom"});
     }
 };
-static_assert(HandlerLike<ErrHandler>);
+
+struct BadHandlerNoID
+{
+    static HandlerExecuteResult execute(const std::vector<std::uint8_t>&, const Communicator&) { return {}; }
+};
+
+struct BadHandlerBadExecute
+{
+    static constexpr std::uint8_t ID = 0x04;
+    // Missing execute method
+};
 
 // For duplicate-ID compile-time uniqueness helper test
-struct Dup1 { static constexpr std::uint8_t ID = 0x7F; static HandlerExecuteResult execute(const std::vector<std::uint8_t>&, const Communicator&) { return {}; } };
-struct Dup2 { static constexpr std::uint8_t ID = 0x7F; static HandlerExecuteResult execute(const std::vector<std::uint8_t>&, const Communicator&) { return {}; } };
-static_assert(HandlerLike<Dup1> && HandlerLike<Dup2>);
+struct Dup1
+{
+    static constexpr std::uint8_t ID = 0x7F;
+    static HandlerExecuteResult execute(const std::vector<std::uint8_t>&, const Communicator&) { return {}; }
+};
+struct Dup2
+{
+    static constexpr std::uint8_t ID = 0x7F;
+    static HandlerExecuteResult execute(const std::vector<std::uint8_t>&, const Communicator&) { return {}; }
+};
+
+TEST(HandlerLikeConcept, DetectIsHandlerLike) {
+    static_assert(HandlerLike<OkHandlerA>, "OkHandlerA should satisfy HandlerLike");
+    static_assert(HandlerLike<OkHandlerB>, "OkHandlerB should satisfy HandlerLike");
+    static_assert(HandlerLike<ErrHandler>, "ErrHandler should satisfy HandlerLike");
+    static_assert(HandlerLike<Dup1>, "Dup1 should satisfy HandlerLike");
+    static_assert(HandlerLike<Dup2>, "Dup2 should satisfy HandlerLike");
+    static_assert(HandlerLike<ErrHandler>, "ErrHandler should satisfy HandlerLike");
+
+    static_assert(!HandlerLike<BadHandlerNoID>, "BadHandlerNoID should not satisfy HandlerLike");
+    static_assert(!HandlerLike<BadHandlerBadExecute>, "BadHandlerBadExecute should not satisfy HandlerLike");
+    SUCCEED();
+}
+TEST(HandlerLikeConcept, DetectsDuplicateIDsAtCompileTime) {
+    static_assert(meta_handler_helpers::UniqueIds<OkHandlerA, OkHandlerB>::value, "IDs should be unique");
+    static_assert(!meta_handler_helpers::UniqueIds<Dup1, Dup2>::value, "IDs should not be unique");
+    SUCCEED();
+}
 
 /* ―――――――――――――――― Tests ―――――――――――――――― */
 
@@ -100,7 +137,7 @@ TEST(MetaHandlerHelpers, UniqueIdsDetectsDuplicatesAtCompileTimeValue) {
 }
 
 TEST(MetaHandlerExecute, DispatchesToMatchingHandlerByPort) {
-    using MH = MetaHandler<OkHandlerA, OkHandlerB>;
+    using TestMetaHandler = MetaHandler<OkHandlerA, OkHandlerB>;
 
     OkHandlerA::_calls = 0;
     OkHandlerB::_calls = 0;
@@ -111,7 +148,7 @@ TEST(MetaHandlerExecute, DispatchesToMatchingHandlerByPort) {
     const std::vector<std::uint8_t> payload{0xDE, 0xAD};
 
     // Call port = OkHandlerB::ID, only B should execute
-    const MetaHandlerExecuteResult res = MH::execute(OkHandlerB::ID, payload, comm);
+    const MetaHandlerExecuteResult res = TestMetaHandler::execute(OkHandlerB::ID, payload, comm);
 
     EXPECT_TRUE(res.ok());
     EXPECT_EQ(OkHandlerA::_calls, 0);
@@ -120,12 +157,12 @@ TEST(MetaHandlerExecute, DispatchesToMatchingHandlerByPort) {
 }
 
 TEST(MetaHandlerExecute, UnknownPortReturnsError) {
-    using MH = MetaHandler<OkHandlerA, OkHandlerB>;
+    using TestMetaHandler = MetaHandler<OkHandlerA, OkHandlerB>;
 
     const DummyCommunicator comm;
     const std::vector<std::uint8_t> payload{0x00};
 
-    const MetaHandlerExecuteResult res = MH::execute(0xFF, payload, comm);
+    const MetaHandlerExecuteResult res = TestMetaHandler::execute(0xFF, payload, comm);
     ASSERT_FALSE(res.ok());
     EXPECT_EQ(res.error().code, META_HANDLER_EXECUTE_STATUS::ERROR_PORT_NOT_FOUND);
     // message should mention the unknown ID
@@ -133,19 +170,18 @@ TEST(MetaHandlerExecute, UnknownPortReturnsError) {
 }
 
 TEST(MetaHandlerExecute, PropagatesHandlerErrorAsMetaError) {
-    using MH = MetaHandler<OkHandlerA, ErrHandler>;
+    using TestMetaHandler = MetaHandler<OkHandlerA, ErrHandler>;
 
     ErrHandler::_calls = 0;
 
     const DummyCommunicator comm;
     const std::vector<std::uint8_t> payload{0xBE, 0xEF};
 
-    const MetaHandlerExecuteResult res = MH::execute(ErrHandler::ID, payload, comm);
+    const MetaHandlerExecuteResult res = TestMetaHandler::execute(ErrHandler::ID, payload, comm);
 
     ASSERT_FALSE(res.ok());
     EXPECT_EQ(ErrHandler::_calls, 1);
     // The meta error code should be the cast of the underlying handler error code
-    EXPECT_EQ(res.error().code,
-              static_cast<META_HANDLER_EXECUTE_STATUS>(HANDLER_EXECUTE_STATUS::ERROR_ID_NOT_FOUND));
+    EXPECT_EQ(res.error().code, static_cast<META_HANDLER_EXECUTE_STATUS>(HANDLER_EXECUTE_STATUS::ERROR_ID_NOT_FOUND));
     EXPECT_EQ(res.error().msg, "boom");
 }
